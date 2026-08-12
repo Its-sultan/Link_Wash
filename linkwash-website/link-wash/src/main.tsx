90d1b998a942e306a4b1df05226d652afa354973
Its-sultan <thabitsultan2002@gmail.com>
2026-08-12 21:00:34 +0300

    feat: added file which is the main entry of the application

---
+++ linkwash-website/link-wash/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode, type ComponentType } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+ 
+
+const Application = App as ComponentType 
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Application/>
+  </StrictMode>
+)
